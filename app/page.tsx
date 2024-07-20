69c7216123d4ee05ee0de1ba14afced45f156081
AyushRay06 <rayayush47@gmail.com>
2024-07-21 01:57:20 +0530

    clerk

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,13 @@
+import { Button } from "@/components/ui/button"
+import { cn } from "@/lib/utils"
 import Image from "next/image"
 
+const state = false
+
 export default function Home() {
-  return <div>Hello</div>
+  return (
+    <div>
+      <Button className={cn("bg-red-50", state && "bg-green-400")}></Button>
+    </div>
+  )
 }
