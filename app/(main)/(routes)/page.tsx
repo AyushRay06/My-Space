b6bc4a89aeabc6e473eb127c01202d21e2b53423
AyushRay06 <rayayush47@gmail.com>
2024-07-21 14:39:33 +0530

    Darkmode

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,13 +1,19 @@
+"use client"
+
+import { ModeToggle } from "@/components/mode-toggle"
 import { Button } from "@/components/ui/button"
 import { cn } from "@/lib/utils"
+import { SignIn, SignInButton, UserProfile } from "@clerk/clerk-react"
+import { UserButton } from "@clerk/nextjs"
 import Image from "next/image"
 
 const state = false
 
 export default function Home() {
   return (
-    <div>
-      <Button className={cn("bg-red-500", state && "bg-green-400")}></Button>
+    <div className="h-full flex items-center justify-center font-bold text-7xl">
+      <UserButton afterSwitchSessionUrl="/" />
+      <ModeToggle />
     </div>
   )
 }
